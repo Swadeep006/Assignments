a31839627d41e394e03fe2cb9648f45229806175
Swadeep <dhondiswadeepdsp6@gmail.com>
2025-05-19 19:36:07 +0530

    compledted arrays and loops part2

--- javascript/scripts/11-todo-list.js
+++ javascript/scripts/11-todo-list.js
@@ -4,19 +4,23 @@ function renderTodoList() {
   let todoListHTML = "";
   for (let i = 0; i < myArray.length; i++) {
     const todoObject = myArray[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
     const { name, dueDate } = todoObject;
     const html = `
-    <p>
-    ${name} ${dueDate}
-    <button onclick="
-    myArray.splice(${i},1);
-    renderTodoList();
-    ">Delete</button>
-    </p>
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        myArray.splice(${i},1);
+        renderTodoList();"
+        class="delete-todo-button"
+        >
+        Delete
+      </button>
     `;
     todoListHTML += html;
   }
-
+  console.log(todoListHTML);
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
 function addButton() {
@@ -24,13 +28,17 @@ function addButton() {
   const dateInputElement = document.querySelector(".js-dueDate");
   const name = inputElem.value;
   const dueDate = dateInputElement.value;
-  myArray.push({
-    //name:name,
-    //dueDate:dueDate
-    name,
-    dueDate,
-  });
+  if (name === "" || dueDate === "") {
+    window.alert("Please Fill Both the Details");
+  } else {
+    myArray.push({
+      //name:name,
+      //dueDate:dueDate
+      name,
+      dueDate,
+    });
 
-  inputElem.value = "";
-  renderTodoList();
+    inputElem.value = "";
+    renderTodoList();
+  }
 }
