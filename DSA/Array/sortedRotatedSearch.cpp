670bcc2b28d99a545a7d3eeddf2f47012f6e33c5
Swadeep <dhondiswadeepdsp6@gmail.com>
2025-06-16 22:49:55 +0530

    solved

---
+++ DSA/Array/sortedRotatedSearch.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int low =0,high = arr.size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>=arr[low]){
+                if(target >= arr[low] && target <= arr[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(target >= arr[mid] && target <= arr[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+        
+    }
+};
